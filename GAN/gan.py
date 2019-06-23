f88be67664121e76f96cca3a44ccd9eb740b45f0
Lornatang <shiyipaisizuo@163.com>
2019-06-23 13:12:26 +0800

    fix bugs

--- GAN/gan.py
+++ GAN/gan.py
@@ -52,9 +52,6 @@ img_shape = (28, 28, 1)
 # to visualize progress in the animated GIF)
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
-# This method returns a helper function to compute cross entropy loss
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
 
 def load_data(buffer_size, batch_size):
   """
@@ -80,11 +77,87 @@ def load_data(buffer_size, batch_size):
   return train_dataset
 
 
+# Both the generator and discriminator are defined using the Keras Sequential API.
+def make_generator_model():
+  """
+
+  Returns:
+    tf.keras.Model
+  """
+  model = tf.keras.Sequential()
+  model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Reshape((7, 7, 256)))
+  assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+
+  model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+  assert model.output_shape == (None, 7, 7, 128)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+  assert model.output_shape == (None, 14, 14, 64)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+  assert model.output_shape == (None, 28, 28, 1)
+
+  return model
+
+
+generator = make_generator_model()
+
+
+# The discriminator is a CNN-based image classifier.
+def make_discriminator_model():
+  """
+
+    Returns:
+      tf.keras.Model
+    """
+  model = tf.keras.Sequential()
+  model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                          input_shape=[28, 28, 1]))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Flatten())
+  model.add(layers.Dense(1))
+
+  return model
+
+
+discriminator = make_discriminator_model()
+
+# Define the loss and optimizers
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+# Generator loss
+def generator_loss(fake_output):
+  return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+# Discriminator loss
 def discriminator_loss(real_output, fake_output):
   real_loss = cross_entropy(tf.ones_like(real_output), real_output)
   fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
   total_loss = real_loss + fake_loss
   return total_loss
+
+
+# The discriminator and the generator optimizers are different since
+# we will train two networks separately.
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 
 class GAN(tf.keras.Model):
@@ -105,15 +178,15 @@ class GAN(tf.keras.Model):
     self.discriminator_optimizer = tf.optimizers.RMSprop(lr=5e-1)
 
     self.gen = tf.keras.Sequential([
-      layers.Dense(units=7 * 7 * 256, input_shape=(noise_dim, )),
+      layers.Dense(units=7 * 7 * 256, input_shape=(noise_dim,)),
       layers.ReLU(),
 
       layers.Reshape(target_shape=(7, 7, 256)),
 
-      layers.Conv2DTranspose(128, (3, 3), strides=(2, 2),
+      layers.Conv2DTranspose(256, (3, 3), strides=(2, 2),
                              padding='same'),
       layers.ReLU(),
-      layers.Conv2DTranspose(64, (3, 3), strides=(2, 2),
+      layers.Conv2DTranspose(128, (3, 3), strides=(2, 2),
                              padding='same'),
       layers.ReLU(),
       layers.Conv2DTranspose(1, (3, 3), strides=(1, 1),
@@ -135,6 +208,7 @@ class GAN(tf.keras.Model):
       layers.Dense(units=1, activation=None)]
     )
 
+  @tf.function
   def generator(self, z):
     return self.gen(z)
 
@@ -175,38 +249,51 @@ class GAN(tf.keras.Model):
       zip(gradients_of_discriminator, self.disc.trainable_variables))
 
 
-# Create the models
-model = GAN()
-
 # Save checkpoints
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=model.generator_optimizer,
-                                 discriminator_optimizer=model.discriminator_optimizer,
-                                 generator=model.gen,
-                                 discriminator=model.disc)
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
 
 # create dir
 if not os.path.exists(checkpoint_dir):
   os.makedirs(checkpoint_dir)
 
 
+# Notice the use of `tf.function`
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+  noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    generated_images = generator(noise, training=True)
+
+    real_output = discriminator(images, training=True)
+    fake_output = discriminator(generated_images, training=True)
+
+    gen_loss = generator_loss(fake_output)
+    disc_loss = discriminator_loss(real_output, fake_output)
+
+  gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+  gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
 def train(dataset, epochs):
-  """ train op
-
-    Args:
-      dataset: mnist dataset or cifar10 dataset.
-      epochs: number of iterative training.
-
-    """
   for epoch in range(epochs):
     start = time.time()
 
     for image_batch in dataset:
-      model.train_step(image_batch)
+      train_step(image_batch)
 
     # Produce images for the GIF as we go
-    generate_and_save_images(model.gen,
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,
                              epoch + 1,
                              seed)
 
@@ -214,10 +301,11 @@ def train(dataset, epochs):
     if (epoch + 1) % 15 == 0:
       checkpoint.save(file_prefix=checkpoint_prefix)
 
-    print(f'Time for epoch {epoch + 1} is {time.time() - start:.3f} sec.')
+    print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
 
   # Generate after the final epoch
-  generate_and_save_images(model.gen,
+  display.clear_output(wait=True)
+  generate_and_save_images(generator,
                            epochs,
                            seed)
 
@@ -279,3 +367,4 @@ def create_gif(file_name):
 if __name__ == '__main__':
   train_images = load_data(BUFFER_SIZE, BATCH_SIZE)
   train(train_images, epochs=50)
+  create_gif('gan.gif')
